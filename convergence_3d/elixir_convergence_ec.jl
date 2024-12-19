b7426d99b602fb660af4ed11959230f2f797404a
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2024-12-19 09:37:49 -0500

    bug fix

--- convergence_3d/elixir_convergence_ec.jl
+++ convergence_3d/elixir_convergence_ec.jl
@@ -121,7 +121,7 @@ alive_callback = AliveCallback(analysis_interval=analysis_interval)
 save_solution = SaveSolutionCallback(interval=100,
                                      save_initial_solution=true,
                                      save_final_solution=true,
-                                     solution_variables=cons2prim
+                                     solution_variables=cons2prim,
                                      output_directory=joinpath(@__DIR__, "out"))
 
 cfl = 0.5
